85f11e541230b3e376815accfdeb2fd10bd718bd
dae <notloneth@gmail.com>
2023-12-13 13:56:14 +0700

    add: Lokasi TPS

--- resources/views/banksampah/indexbs.blade.php
+++ resources/views/banksampah/indexbs.blade.php
@@ -4,6 +4,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -33,11 +34,14 @@
             margin-top: 20px;
         }
 
-        table, th, td {
+        table,
+        th,
+        td {
             border: 1px solid #ddd;
         }
 
-        th, td {
+        th,
+        td {
             padding: 12px;
             text-align: left;
         }
@@ -47,6 +51,7 @@
         }
     </style>
 </head>
+
 <body>
     <div class="container mx-1 col-md-8">
         <div class="top">
@@ -82,6 +87,7 @@
 
             <!DOCTYPE html>
             <html>
+
             <head>
                 <!-- Bootstrap CSS -->
                 <link rel="stylesheet" href="{{ asset('css/maps.css') }}">
@@ -96,6 +102,7 @@
                     }
                 </style>
             </head>
+
             <body>
                 <div id="map-canvas"></div>
                 <script>
@@ -103,8 +110,45 @@
                     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                         attribution: '&copy; <a href="https://www.google.com/maps/@-7.7860654,110.377561,18.55z?entry=ttu">OpenStreetMap</a> contributors'
                     }).addTo(map);
+
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        var lat = position.coords.latitude;
+                        var lon = position.coords.longitude;
+
+                        map.setView([lat, lon], 18);
+                        var userLocation = L.marker([lat, lon]).addTo(map);
+                        userLocation.bindPopup('Kamu Disini!').openPopup();
+                    });
+
+                    var iconUrls = [
+                        'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+                        'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+                        'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+                        'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+                    ];
+
+                    var locations = <?php echo json_encode($datas); ?>;
+
+                    locations.forEach(e => {
+                        var randomIconUrl = iconUrls[Math.floor(Math.random() * iconUrls.length)];
+
+                        var greenIcon = new L.Icon({
+                            iconUrl: randomIconUrl,
+                            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                            iconSize: [25, 41],
+                            iconAnchor: [12, 41],
+                            popupAnchor: [1, -34],
+                            shadowSize: [41, 41]
+                        });
+
+                        L.marker([
+                            e.latitude,
+                            e.longitude
+                        ], {
+                            icon: greenIcon
+                        }).addTo(map).bindPopup(e.name).openPopup();
+                    });
                 </script>
-                <!-- Bootstrap JS -->
                 <script src="{{ asset('js/buang.js') }}"></script>
             </body>
 
@@ -115,5 +159,6 @@
     <!-- Bootstrap JS -->
     <script src="{{ asset('js/maps.js') }}"></script>
 </body>
+
 </html>
 @endsection
